ef605fa546fb19274a13c11ba6b09a2fafe34053
MCLopezMoreno <2013mariana.098@gmail.com>
2025-07-16 11:51:26 -0500

    Include "lastname" into Login response

--- solvex/src/auth/auth.service.ts
+++ solvex/src/auth/auth.service.ts
@@ -103,6 +103,7 @@ export class AuthService {
       email: findUser.email,
       id_role: findUser.user.role.id_role,
       name: findUser.user.name,
+      lastname: findUser.user.lastname,
       phone: findUser.user.phone,
       identification_number: findUser.user.identification_number,
     };

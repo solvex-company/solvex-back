1fe7f81ffd6fefd20bbeca5cd7ddaec97f7d46e4
AVLopezSanchez <andrea.virginia.ls@gmail.com>
2025-07-22 17:38:55 -0400

    add status and employes to getTicketById

--- solvex/src/tickets/tickets.service.ts
+++ solvex/src/tickets/tickets.service.ts
@@ -184,7 +184,7 @@ export class TicketsService {
   async getTicketById(ticketId: number) {
     const ticketFound: Ticket | null = await this.ticketRepository.findOne({
       where: { id_ticket: ticketId },
-      relations: ['id_empleado', 'id_status'],
+      relations: ['id_empleado', 'id_status', 'id_helper'],
     });
 
     if (!ticketFound)

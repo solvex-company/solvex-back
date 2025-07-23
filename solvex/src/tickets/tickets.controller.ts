e6a627c64b48edb6c31fe7eacdcaaae077eed532
LuisMendez <lmiguel05c@gmail.com>
2025-07-23 10:12:02 -0500

    cambio de role en getAllTickets

--- solvex/src/tickets/tickets.controller.ts
+++ solvex/src/tickets/tickets.controller.ts
@@ -59,7 +59,7 @@ export class TicketsController {
   }
 
   @Get('getAllTickets')
-  @Roles(Role.HELPER, Role.ADMIN)
+  @Roles(Role.EMPLOYEE, Role.HELPER, Role.ADMIN)
   @UseGuards(AuthGuard, RolesGuard)
   getAllTickets() {
     return this.ticketsService.getAllTickets();

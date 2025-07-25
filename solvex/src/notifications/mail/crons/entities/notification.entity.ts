af0ed9e79c1bf6bb0017913150748ef82fd4bcb6
tobigonzalez99 <tobias99gonzalez@gmail.com>
2025-07-25 13:50:11 -0300

    entidad notificaciones crons

---
+++ solvex/src/notifications/mail/crons/entities/notification.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, CreateDateColumn } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Ticket } from 'src/tickets/entities/ticket.entity';
+
+@Entity({ name: 'notificaciones' })
+export class Notification {
+  @PrimaryGeneratedColumn({ name: 'id_notificacion', type: 'int' })
+  id: number;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'id_usuario' })
+  user: User;
+
+  @ManyToOne(() => Ticket)
+  @JoinColumn({ name: 'id_ticket' })
+  ticket: Ticket;
+
+  @Column({ name: 'mensaje', type: 'varchar', length: 255, nullable: false })
+  message: string;
+
+  @Column({ name: 'leida', type: 'boolean', default: false })
+  read: boolean;
+
+  @CreateDateColumn({ name: 'fecha_creacion' })
+  createdAt: Date;
+} 

2575b350f994fe9db7a10e382254c226c50d3c80
diego <diegofbacag@gmail.com>
2025-07-29 16:19:59 -0500

    update change password

--- solvex/src/auth/changePassword.dto.ts
+++ solvex/src/auth/changePassword.dto.ts
@@ -1,25 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsStrongPassword } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsStrongPassword,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class ChangePasswordDto {
   @ApiProperty({
     description: 'constraseña antigua',
     example: 'User123!',
   })
-  @IsNotEmpty()
-  @IsStrongPassword({
-    minLowercase: 1,
-    minUppercase: 1,
-    minNumbers: 1,
-    minSymbols: 1,
-  })
-  oldPassword: string;
+  oldPassword: string | null;
 
   @ApiProperty({
     description: 'constraseña nueva',
     example: 'Usernew123!',
   })
   @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(15)
   @IsStrongPassword({
     minLowercase: 1,
     minUppercase: 1,
@@ -33,6 +33,8 @@ export class ChangePasswordDto {
     example: 'Usernew123!',
   })
   @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(15)
   @IsStrongPassword({
     minLowercase: 1,
     minUppercase: 1,
